724452d2f0bc8953700411bc63721da7adeceb8f
kizhuravlev <krl.zhuravlev@proton.me>
2026-07-04 13:40:21 +0300

    create course client dependencies fixed

--- httpx_module/api_client_create_course.py
+++ httpx_module/api_client_create_course.py
@@ -1,3 +1,4 @@
+from httpx_module.clients.users.private_users_client import LoginRequestDict
 from httpx_module.clients.users.public_users_client import get_public_users_client, CreateUserRequestDict
 from httpx_module.tools.fakers import random_user_email
 from httpx_module.clients.files.files_client import get_files_client, CreateFileRequestDict
