d5f8e42310eb1653769b069982f833cbbae56ff6
kizhuravlev <krl.zhuravlev@proton.me>
2026-07-06 19:16:32 +0300

    pydantic create user fixed

--- httpx_module/pydantic_create_user.py
+++ httpx_module/pydantic_create_user.py
@@ -1,18 +1,21 @@
 from pydantic import BaseModel, Field, EmailStr, UUID4
 
+STR_LENGTH_VALIDATOR = {"min_length": 1, "max_length": 50}
+PASSWORD_LENGTH_VALIDATOR = {"min_length": 1, "max_length": 250}
+
 class UserSchema(BaseModel):
     id: UUID4
     email: EmailStr
-    last_name: str = Field(min_length=1, max_length=50, alias="lastName")
-    first_name: str = Field(min_length=1, max_length=50, alias="firstName")
-    middle_name: str = Field(min_length=1, max_length=50, alias="middleName")
+    last_name: str = Field(**STR_LENGTH_VALIDATOR, alias="lastName")
+    first_name: str = Field(**STR_LENGTH_VALIDATOR, alias="firstName")
+    middle_name: str = Field(**STR_LENGTH_VALIDATOR, alias="middleName")
 
 class CreateUserRequestSchema(BaseModel):
     email: EmailStr
-    password: str = Field(min_length=1, max_length=250)
-    last_name: str = Field(min_length=1, max_length=50, alias="lastName")
-    first_name: str = Field(min_length=1, max_length=50, alias="firstName")
-    middle_name: str = Field(min_length=1, max_length=50, alias="middleName")
+    password: str = Field(**PASSWORD_LENGTH_VALIDATOR)
+    last_name: str = Field(**STR_LENGTH_VALIDATOR, alias="lastName")
+    first_name: str = Field(**STR_LENGTH_VALIDATOR, alias="firstName")
+    middle_name: str = Field(**STR_LENGTH_VALIDATOR, alias="middleName")
 
 class CreateUserResponseSchema(BaseModel):
     user: UserSchema
