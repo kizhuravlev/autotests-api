0b601eeafaede99aca2bf56ea307d8d89efaad30
kizhuravlev <krl.zhuravlev@proton.me>
2026-09-06 21:55:00 +0300

    feat: test_get_exercise added

--- httpx_module/tools/assertions/exercises.py
+++ httpx_module/tools/assertions/exercises.py
@@ -1,4 +1,5 @@
-from httpx_module.clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from httpx_module.clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+    ExerciseSchema, GetExerciseResponseSchema
 from httpx_module.tools.assertions.base import assert_equal
 
 
@@ -17,3 +18,24 @@ def assert_create_exercise_response(request: CreateExerciseRequestSchema, respon
     assert_equal(response.exercise.order_index, request.order_index, "order_index")
     assert_equal(response.exercise.description, request.description, "description")
     assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
+
+
+def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
+    """
+    Проверяет, что ответ на получение задания соответствует ответу создания задания.
+
+    :param actual: Ответ от API на получение задания
+    :param expected: Ответ от API на создание задания
+    :raises AssertionError: Ответ на создание задания не соответствует запросу
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.title, expected.title, "title")
+    assert_equal(actual.course_id, expected.course_id, "course_id")
+    assert_equal(actual.max_score, expected.max_score, "max_score")
+    assert_equal(actual.min_score, expected.min_score, "min_score")
+    assert_equal(actual.order_index, expected.order_index, "order_index")
+    assert_equal(actual.description, expected.description, "description")
+    assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
+
+def assert_get_exercise_response(get_exercise_response: GetExerciseResponseSchema, create_exercise_response: CreateExerciseResponseSchema):
+    assert_exercise(get_exercise_response.exercise, create_exercise_response.exercise)
--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -3,10 +3,12 @@ from http import HTTPStatus
 import pytest
 
 from fixtures.courses import CourseFixture
+from fixtures.exercises import ExerciseFixture
 from httpx_module.clients.exercises.exercises_client import ExercisesClient
-from httpx_module.clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from httpx_module.clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+    GetExerciseResponseSchema
 from httpx_module.tools.assertions.base import assert_status_code
-from httpx_module.tools.assertions.exercises import assert_create_exercise_response
+from httpx_module.tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response
 from httpx_module.tools.assertions.schema import validate_json_schema
 
 
@@ -22,3 +24,10 @@ class TestExercises:
         assert_create_exercise_response(request, response_data)
 
         validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_get_exercise(self, exercises_client: ExercisesClient, function_exercise: ExerciseFixture):
+        response = exercises_client.get_exercise_api(function_exercise.response.exercise.id)
+        response_data = GetExerciseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_get_exercise_response(response_data, function_exercise.response)
