4e87ec215a37f3532481f7e4987a96c3481e9869
kizhuravlev <krl.zhuravlev@proton.me>
2026-09-06 22:38:14 +0300

    feat: added test_update_exercise and connected assertions

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -6,9 +6,10 @@ from fixtures.courses import CourseFixture
 from fixtures.exercises import ExerciseFixture
 from httpx_module.clients.exercises.exercises_client import ExercisesClient
 from httpx_module.clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
-    GetExerciseResponseSchema
+    GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
 from httpx_module.tools.assertions.base import assert_status_code
-from httpx_module.tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response
+from httpx_module.tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
+    assert_update_exercise_response
 from httpx_module.tools.assertions.schema import validate_json_schema
 
 
@@ -31,3 +32,15 @@ class TestExercises:
 
         assert_status_code(response.status_code, HTTPStatus.OK)
         assert_get_exercise_response(response_data, function_exercise.response)
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_update_exercise(self, exercises_client: ExercisesClient, function_exercise: ExerciseFixture):
+        request = UpdateExerciseRequestSchema()
+        response = exercises_client.update_exercise_api(function_exercise.response.exercise.id, request)
+        response_data = UpdateExerciseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_update_exercise_response(request, response_data)
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
